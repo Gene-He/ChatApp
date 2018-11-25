8f3bac060167a1126c15167d8d79acdf74ec03cd
jrxccf <785492085@qq.com>
2018-11-25 09:47:27 -0600

    update notifications of ChatRoom

--- src/main/java/edu/rice/comp504/model/obj/ChatRoom.java
+++ src/main/java/edu/rice/comp504/model/obj/ChatRoom.java
@@ -149,6 +149,7 @@ public class ChatRoom extends Observable {
         }
         notifyObservers(new JoinRoomCmd("join,"+id));
         userNameFromUserId.put(user.getId(),user.getName());
+        notifications.add(user.getName()+ "joined this room");
         addObserver(user);
         return true;
     }
@@ -165,6 +166,7 @@ public class ChatRoom extends Observable {
         }
         userNameFromUserId.remove(user.getId());
         notifyObservers(new LeaveRoomCmd("leave,"+id));
+        notifications.add(user.getName()+ "left this room because " + reason);
         deleteObserver(user);
         return true;
     }
