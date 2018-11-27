5cb76ef5090071752365beaa98bfebfed9cae5e0
jc129 <jc129@rice.edu>
2018-11-26 17:52:23 -0800

    Comments in WebScoketController

--- src/main/java/edu/rice/comp504/controller/WebSocketController.java
+++ src/main/java/edu/rice/comp504/controller/WebSocketController.java
@@ -20,7 +20,6 @@ public class WebSocketController {
      */
     @OnWebSocketConnect
     public void onConnect(Session user) {
-
     }
 
 
@@ -48,12 +47,14 @@ public class WebSocketController {
             sendMessageAction(user, message);
         } else if (info[0].equals("ack")) {
             ackMessageAction(user, message);
-        } else if (info[0].equals("query") && info[1].equals("roomUsers")) {
-            queryUsersOfRoomAction(user, message);
-        } else if (info[0].equals("query") && info[1].equals("roomNotifications")) {
-            queryNotificationsOfRoomAction(user, message);
-        } else if (info[0].equals("query") && info[1].equals("userChatHistory")) {
-            queryChatHistoryOfRoomAction(user, message);
+        } else if (info.length > 1) {
+            if (info[0].equals("query") && info[1].equals("roomUsers")) {
+                queryUsersOfRoomAction(user, message);
+            } else if (info[0].equals("query") && info[1].equals("roomNotifications")) {
+                queryNotificationsOfRoomAction(user, message);
+            } else if (info[0].equals("query") && info[1].equals("userChatHistory")) {
+                queryChatHistoryOfRoomAction(user, message);
+            }
         }
     }
 
@@ -70,7 +71,10 @@ public class WebSocketController {
      * We define all types of actions for different requests.
      * We can directly call dispatcher's method in onMessage(), but these separate actions make different services clear and give us a chance to check
      * if the message is in required format before actually calling dispatcher.
-     *
+     * */
+
+    /**
+     * user acknowledge receipt of message
      * @Throws throw {@code IllegalArgumentException} if this message is not in required format.
      */
     private void ackMessageAction(Session user, String message) {
@@ -78,46 +82,91 @@ public class WebSocketController {
         ChatAppController.getDispatcher().ackMessage(user, message.trim());
     }
 
+    /**
+     * Handle user creates a new chat room
+     * @param user
+     * @param message
+     */
     private void addRoomAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 6, "Illegal create room message format: %s", message);
         ChatAppController.getDispatcher().loadRoom(user, message.trim());
     }
 
+    /**
+     * modify chat room
+     * @param user
+     * @param message
+     */
     private void enforceFilterAction(Session user, String message) {
-//        Preconditions.checkArgument(message.split(" ").length == 6, "Illegal modify room message format: %s", message);
-//        ChatAppController.getDispatcher().modifyRoom(user, message.trim());
+        Preconditions.checkArgument(message.split(" ").length == 6, "Illegal modify room message format: %s", message);
+        ChatAppController.getDispatcher().modifyRoom(user, message.trim());
     }
 
+    /**
+     * User joins a room
+     * @param user
+     * @param message
+     */
     private void joinRoomAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 2, "Illegal join room message format: %s", message);
         ChatAppController.getDispatcher().joinRoom(user, message);
     }
 
+    /**
+     * User leaves a room
+     * @param user
+     * @param message
+     */
     private void leaveRoomAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 2, "Illegal leave room message format: %s", message);
         ChatAppController.getDispatcher().leaveRoom(user, message);
     }
 
+    /**
+     * user query for chat history
+     * @param user
+     * @param message
+     */
     private void queryChatHistoryOfRoomAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 4, "Illegal QueryChatHistoryOfRoom message format: %s", message);
         ChatAppController.getDispatcher().query(user, message);
     }
 
+    /**
+     * user query for notification of room
+     * @param user
+     * @param message
+     */
     private void queryNotificationsOfRoomAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 3, "Illegal QueryNotificationsOfRoom message format: %s", message);
         ChatAppController.getDispatcher().query(user, message);
     }
 
+    /**
+     * user query for other users in a chat room
+     * @param user
+     * @param message
+     */
     private void queryUsersOfRoomAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 3, "Illegal QueryUsersOfRoom message format: %s", message);
         ChatAppController.getDispatcher().query(user, message);
     }
 
+    /**
+     * user sends a message
+     * @param user
+     * @param message
+     */
     private void sendMessageAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 4, "Illegal sendMessage message format: %s", message);
         ChatAppController.getDispatcher().sendMessage(user, message);
     }
 
+    /**
+     * handle user login
+     * @param user
+     * @param message
+     */
     private void loginAction(Session user, String message) {
         Preconditions.checkArgument(message.split(" ").length == 5, "Illegal login message format: %s", message);
         ChatAppController.getDispatcher().loadUser(user, message);
