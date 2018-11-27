42ff4a298c3ad2e903fe418fa6f830f7fdd39a4a
jialihan <jialihan@handembp.dyndns.rice.edu>
2018-11-26 22:00:49 -0600

    window close bug fix

--- src/main/resources/public/js/chatapp.js
+++ src/main/resources/public/js/chatapp.js
@@ -35,24 +35,32 @@ function updateChatRoom(message) {
 
 }
 
+/**
+ * Send Login Request with User Info to Server
+ */
 function createUserInfo()
 {
-    setTimeout(function() { $('#myModal').modal('hide'); }, 4000);
+    setTimeout(function() {
+        document.getElementById("login_close").click();
+    }, 4000);
     var uname = document.getElementById("reg_username").value;
     var age = document.getElementById("reg_age").value;
     var loc = document.getElementById("reg_location").value;
     var sch =  document.getElementById("reg_school").value;
     // Grammar: login [userName] [age] [location] [school]
     var user_str = "login "+uname +" "+ age +" "+ loc+" " + sch;
-   // alert(user_str);
-    sendMessage(user_str);
-   // document.getElementById("loginUserResult").innerHTML = "Login Success!";
+     sendMessage(user_str);
 
 }
 
+/**
+ * Send Create Room Request with Room Info to Server
+ */
 function createRoomInfo()
 {
-    setTimeout(function() { $('#roomModal').modal('hide'); }, 4000);
+    setTimeout(function() {
+        document.getElementById("createroom_close").click(); // Click on the checkbox
+    }, 4000);
     var rname = document.getElementById("reg_roomname").value;
     var minage = document.getElementById("reg_minage").value;
     var maxage = document.getElementById("reg_maxage").value;
@@ -61,7 +69,5 @@ function createRoomInfo()
     // Grammar: create [roomName] [ageLower] [ageUpper] f[location],g*f[location]g f[school],g*f[school]g
     var room_str = "create "+rname +" "+ minage +" "+ maxage+ " " + r_loc +" " + r_sch;
 
-    //alert(room_str);
     sendMessage(room_str);
-    //document.getElementById("createRoomResult").innerHTML = "Create Success!";
 }
